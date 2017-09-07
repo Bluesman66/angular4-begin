c7672ab50758e78037dfa89d95c33a4fb60f0e47
alex.paclin <alex.paclin@gmail.com>
2017-09-07 14:29:55 +0500

    123 Configuring Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,57 @@
 import { AppErrorHandler } from './common/app-error-handler';
 import { ErrorHandler } from '@angular/core';
 import { PostService } from './services/post.service';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { HomeComponent } from './home/home.component';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 import { PostsComponent } from './posts/posts.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
-  ],
+    PostsComponent,
+    NavbarComponent,
+    HomeComponent,    
+    GithubFollowersComponent, 
+    NotFoundComponent, 
+    GithubProfileComponent
+  ],  
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule    
+    HttpModule,
+    RouterModule.forRoot([
+      { 
+        path: '',
+        component: HomeComponent 
+      },
+      { 
+        path: 'followers/:username', 
+        component: GithubProfileComponent 
+      },
+      { 
+        path: 'followers', 
+        component: GithubFollowersComponent 
+      },      
+      { 
+        path: 'posts', 
+        component: PostsComponent 
+      },
+      { 
+        path: '**', 
+        component: NotFoundComponent 
+      }      
+    ])    
   ],
   providers: [
     PostService,
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -5,7 +5,7 @@ import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'posts',
+  selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
